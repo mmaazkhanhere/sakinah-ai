6304f8c39bf9b7443691d97cbfc0b15da104e417
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-07-22 14:14:33 +0500

    update: updating the schema of agent

--- src/agent/schema.py
+++ src/agent/schema.py
@@ -1,10 +1,31 @@
 from typing import Annotated, Dict
 from typing_extensions import TypedDict
 
-from langgraph.graph.message import add_messages
+from pydantic import BaseModel, Field
+
+
+class QuranAyah(BaseModel):
+    surah_no: int = Field(description="The surah number of the ayah")
+    surah_name: str = Field(description="The name of the surah")
+    ayah_no_surah: int = Field(description="The ayah number in the surah")
+    ayah_eng: str = Field(description="The English translation of the ayah")
+    ayah_arabic: str = Field(description="The Arabic text of the ayah")
+
+class Hadith(BaseModel):
+    hadith: str = Field(description="The text of the hadith")
+    hadith_number: int = Field(description="The number of the hadith")
+    narrator: str = Field(description="The narrator of the hadith")
+    book_reference: str = Field(description="The book reference of the hadith")
+
+
+
+class AgentResponse(BaseModel):
+    answer: str = Field(description="The response of AI")
+    quran_ayah: QuranAyah = Field(description="The quran ayah")
+    hadith: Hadith = Field(description="The hadith")
 
 class AgentState(TypedDict):
     user_message: str  #the message entered by the user
     context: list[str] # context the is  built using rag
     chat_history: list[Dict[str, str]] # history of chat between the agent and the user
-    answer: str # generated the answer by the LLM
+    response: AgentResponse # generated the answer by the LLM
